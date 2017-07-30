2586be774852561e5949234c195d28ce779051de
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-07-30 23:14:22 +0530

    Update FileHandling.java

--- OOPSession5/FileHandling.java
+++ OOPSession5/FileHandling.java
@@ -1,3 +1,6 @@
+/**
+ * class to read files having users and connections
+ */
 package SocialNetwork;
 
 import java.io.BufferedReader;
@@ -8,16 +11,21 @@ public class FileHandling {
 	FileInputStream fstream;
 	BufferedReader br;
 	String str = "";
-	StringBuffer buf = new StringBuffer();
-	
-	public String read(String filepath){
+	StringBuffer buf;
+
+	/**
+	 * 
+	 * @param filepath
+	 * @return a string having all values
+	 */
+	public String read(String filepath) {
 		try {
+			buf = new StringBuffer();
 			fstream = new FileInputStream(filepath);
 			br = new BufferedReader(new InputStreamReader(fstream));
 			while ((str = br.readLine()) != null) {
 				buf.append(str);
 				buf.append("\n");
-				/**/
 			}
 			br.close();
 		} catch (Exception e) {
@@ -25,6 +33,5 @@ public class FileHandling {
 		}
 		return buf.toString();
 	}
-	
-	
+
 }
