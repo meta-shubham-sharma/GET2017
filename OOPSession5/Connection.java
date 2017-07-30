dea9a718c123148e4aada36ff25d56ca4ae6fa9b
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-07-30 22:33:20 +0530

    Update Connection.java

--- OOPSession5/Connection.java
+++ OOPSession5/Connection.java
@@ -2,7 +2,9 @@ package SocialNetwork;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Connection {
 
@@ -10,11 +12,10 @@ public class Connection {
 
 	public Connection(String file) {
 		String stringOutput[] = file.split("\n");
-		String str[] = {};
-		List<String> listOfFriends = new ArrayList<String>();
 		for (int index = 0; index < stringOutput.length; index++) {
-			str = stringOutput[index].split(",");
-			for (int jindex = 1; jindex < str[index].length(); jindex++) {
+			List<String> listOfFriends = new ArrayList<String>();
+			String str[] = stringOutput[index].split(",");
+			for (int jindex = 1; jindex < str.length; jindex++) {
 				listOfFriends.add(str[jindex]);
 			}
 			connectionMap.put(str[0], listOfFriends);
@@ -23,19 +24,51 @@ public class Connection {
 
 	public void addConnection(String friendId, String userID) {
 		// TODO Auto-generated method stub
-
 		if (connectionMap.containsKey((userID))) {
 			if (connectionMap.get(userID).contains(friendId)) {
-				System.out.println("this is already your friend");
+				System.out.println("Already your friend");
 			} else {
 				connectionMap.get(userID).add(friendId);
+				if (connectionMap.containsKey((friendId))) {
+					connectionMap.get(friendId).add(userID);
+				} else {
+					List<String> list = new ArrayList<String>();
+					list.add(userID);
+					connectionMap.put(friendId, list);
+				}
 			}
-
 		} else {
 			List<String> list = new ArrayList<String>();
 			list.add(friendId);
-			connectionMap.put(userID,list);
+			connectionMap.put(userID, list);
 		}
 	}
 
+	public void removeConnection(String friendId, String userID) {
+		// TODO Auto-generated method stub
+		if (connectionMap.get(userID).contains(friendId)) {
+			connectionMap.get(userID).remove(friendId);
+			connectionMap.get(friendId).remove(userID);
+			System.out.println("Connection successfully removed");
+		} else {
+			System.out.println("Id does not exist in your Friend List ");
+		}
+	}
+
+	public void printConnections(String userId) {
+		if (connectionMap.containsKey(userId)
+				&& (!connectionMap.get(userId).isEmpty())) {
+			System.out.println(userId + " = " + connectionMap.get(userId));
+		} else {
+			System.out.println("No Friends available");
+		}
+	}
+
+	public void printNetwork() {
+		Iterator itr = connectionMap.entrySet().iterator();
+		while (itr.hasNext()) {
+			Map.Entry pair = (Map.Entry) itr.next();
+			System.out.println(pair.getKey() + " = " + pair.getValue());
+		}
+	}
 }
