8e23d470b5490cacecb54b1dcfe722cd09eaf92a
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-08-06 19:31:27 +0530

    Update MyScript.js

--- JavaScriptSession1/MyScript.js
+++ JavaScriptSession1/MyScript.js
@@ -1,17 +1,41 @@
 function toHome(){
-	document.getElementById("home").href="Assignment1.html";
+	window.location.href="Assignment1.html";
 }
+
 function toContact(){
-	document.getElementById("contact").href="Assignment2.html";
+	window.location.href="Assignment2.html";
 }
-function validateName(name)
+
+function validateForm(){
+	if(validateName())
+	{
+		if(validateEmail())
+		{
+			if(validateCity())
+			{
+				if(validateOrganization())
+				{
+					if(validateNumber())
+					{
+						if(validateMessage())
+						{
+							return true;
+						}
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+function validateName()
 {
 	var name = document.form.name;
 	var len = name.value.length;
 	var letters1 = /^[A-Za-z]+ +[A-Za-z]+$/;
 	var letters2 = /^[A-Za-z]+$/;
 	var letters3 = /^[A-Za-z]+ +[A-Za-z]+ +[A-Za-z]+$/;
-	name.style.outlineColor="green";
+
 	if( ( name.value.match(letters1) || name.value.match(letters2) || name.value.match(letters3) )  && len >1  && len<=20 ){
 		return true;
 	}	
@@ -24,11 +48,12 @@ function validateName(name)
 		return false;
 	}
 }
+
 function validateEmail()
 {
 	var email= document.form.email;
 	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
-	email.style.outlineColor="green";
+	
 	if(email.value.match(mailformat) )
 	{
 		return true;
@@ -42,28 +67,29 @@ function validateEmail()
 		return false;
 	}
 }
+
 function validateOrganization()
 {
 	var organization = document.form.organization;
 	var letters = /^[A-Za-z]+$/;
-	organization.style.outlineColor="green";
 	if(organization.value.match(letters)){
 		return true;
 	}	
 	else{
 		organization.value="";
 		organization.style.outlineColor="red";
-		alert("organization name should have only charachters");
+		alert("Organization name should have only charachters");
 		organization.focus();
 		return false;
 	}
 }
+
 function validateNumber()
 {
 	var num = document.form.num;
 	var numbers = /^[0-9]+$/;  
 	var len = num.value.length;
-	num.style.outlineColor="green";
+
 	if(num.value.match(numbers) && len==10 )
 	{
 		return true;
@@ -77,20 +103,22 @@ function validateNumber()
 		return false;
 	}
 }
+
 function validateMessage()
 {
 	var msg = document.form.msg;
-	msg.style.outlineColor="green";
+
 	if(msg.value.length > 250 || msg.value.length == 0)
 	{
 		msg.value="";
 		msg.style.outlineColor="red";
 		alert("Enter 1 to 250 charachters for message");
-		msg.focus();
+	msg.focus();
 		return false;
 	}
 	return true;
 }
+
 function validateCity()
 {
 	var city = document.form.city;
@@ -102,24 +130,10 @@ function validateCity()
 	fillField(city);
 	return true;
 }
+
 function fillField(city)
 {
 	var name = city.value;
+	document.form.cityname.hidden = false;   
 	document.form.cityname.value = "Selected city is :" + name;
-}
-function checkForm(){
-	var arr = document.forms["form"].elements;
-	var canSubmit = true;
-	document.form.submit.disabled = true;
-	for ( var index = 0; index < arr.length ; index ++)
-	{
-		if(arr[index].value.length == 0)
-		{
-			canSubmit=false;
-		}
-	}
-	if(canSubmit)
-	{
-		document.form.submit.disabled = false;
-	}
 }
