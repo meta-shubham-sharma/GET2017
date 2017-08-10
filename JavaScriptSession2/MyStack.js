6ab932ff0cb60511f9b05dd9fa12e57854babfc5
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-08-10 13:31:05 +0530

    Update MyStack.js

--- JavaScriptSession2/MyStack.js
+++ JavaScriptSession2/MyStack.js
@@ -1,20 +1,51 @@
-var arr = [];
-var topVal=-1;
-function pushItem(){
-	var input = document.getElementById("push").value;
-	document.getElementById("push").value="";
-	arr[++topVal] = input;
-	console.log(arr[topVal] + " " + topVal);
+function Node(data){
+	this.data = data;
+	this.prev=null;
 }
 
-function popItem(){
-	document.getElementById("spanPop").innerHTML = arr[topVal--];
-	console.log(topVal);
-}
+var stack = new MyStack();
 
-function printStack(){
-	document.getElementById("spanStack").innerHTML="";
-	for(var index=0;index <= topVal ; index++){
-		document.getElementById("spanStack").innerHTML += arr[index] + " ";
+ function MyStack(){
+		this.top=null;
+		var temp,data;
+	
+	MyStack.prototype.pushItem = function(){
+		data = document.getElementById("push").value;
+		document.getElementById("push").value = "";
+		var node = new Node(data);
+		node.prev = this.top;
+		this.top=node;
+	}	
+	
+	MyStack.prototype.popItem = function(){
+		document.getElementById("spanPop").innerHTML = "";
+		if(this.top){
+			document.getElementById("spanPop").innerHTML = this.top.data;
+			this.top=this.top.prev;
+		}
+		else{
+			document.getElementById("spanPop").innerHTML = "There are no elements in stack";
+		}
 	}
-}
+ 
+	MyStack.prototype.printStack = function(){
+		temp = this.top;
+		document.getElementById("spanStack").innerHTML="";
+		while(temp!=null){
+			document.getElementById("spanStack").innerHTML += temp.data + " ";
+			temp=temp.prev;
+		}
+	}
+ }
+ 
+ function pushVal(){
+	stack.pushItem();
+ }
+ 
+ function popVal(){
+	stack.popItem();
+ }
+ 
+ function printVal(){
+	stack.printStack();
+ }
