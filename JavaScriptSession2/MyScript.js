8bb5e846966f05e9368a735dfcbd6b01e42de82c
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-08-10 18:55:53 +0530

    Add files via upload

---
+++ JavaScriptSession2/MyScript.js
@@ -0,0 +1,20 @@
+function callStack(){
+
+document.getElementById("content").innerHTML='<object type="text/html" data="Stack.html" width="100%" height="700px" ></object>';
+}
+function callQueue(){
+
+document.getElementById("content").innerHTML='<object type="text/html" data="Queue.html" width="100%" height="700px" ></object>';
+}
+function callSingly(){
+
+document.getElementById("content").innerHTML='<object type="text/html" data="LinkList.html"  width="100%" height="700px" ></object>';
+}
+function callDoubly(){
+
+document.getElementById("content").innerHTML='<object type="text/html" data="DoublyLinkList.html"  width="100%" height="700px"></object>';
+}
+function callRemove(){
+
+document.getElementById("content").innerHTML='<object type="text/html" data="RemoveStrings.html"  width="100%" height="700px"></object>';
+}
