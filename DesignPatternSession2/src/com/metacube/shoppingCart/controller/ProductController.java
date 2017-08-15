5fb33883d12838727de701830d4a7bc0409fd71e
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-08-15 14:41:51 +0530

    Add files via upload

--- DesignPatternSession2/src/com/metacube/shoppingCart/controller/ProductController.java
+++ DesignPatternSession2/src/com/metacube/shoppingCart/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.metacube.shoppingCart.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
 import com.metacube.shoppingCart.Dao.InMemoryProductDao;
 import com.metacube.shoppingCart.Enums.Status;
 import com.metacube.shoppingCart.Facade.ProductFacade;
 import com.metacube.shoppingCart.Model.Product;
+import com.metacube.shoppingCart.Model.ShoppingCart;
 import com.metacube.shoppingCart.View.DisplayOutput;
 
 /**
@@ -18,23 +22,33 @@ public class ProductController {
 	 * Gets the view object.
 	 *
 	 * @param input
-	 *            
+	 * 
 	 * @return the view object
 	 */
 	public void getViewObject(Product input) {
 		String code = input.getCode();
 		if (pfacade.checkProductCode(code)) {
 			pfacade.addToCart(input);
+			DisplayOutput.checkStatus(Status.ADDED);
 		} else {
 			DisplayOutput.checkStatus(Status.NOTAVAILABLE);
 		}
 	}
-	
-	public void removeItem(String code){
-		if (pfacade.checkProductCode(code)) {
+
+	public void removeItem(String code) {
+		if (pfacade.checkProductCodeFromCart(code)) {
 			InMemoryProductDao.removeFromCart(code);
+			DisplayOutput.checkStatus(Status.REMOVED);
 		} else {
 			DisplayOutput.checkStatus(Status.NOTSELECTED);
 		}
+	}
+
+	public List<ShoppingCart> getCart() {
+		return pfacade.getCart();
+	}
+
+	public HashMap<String, Product> getProducts() {
+		return pfacade.getProducts();
 	}
 }
