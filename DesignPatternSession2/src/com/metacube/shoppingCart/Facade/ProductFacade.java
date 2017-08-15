ca48f7957a8acedc94b0932e14f0bfa66f7fbd98
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-08-15 14:39:09 +0530

    Add files via upload

--- DesignPatternSession2/src/com/metacube/shoppingCart/Facade/ProductFacade.java
+++ DesignPatternSession2/src/com/metacube/shoppingCart/Facade/ProductFacade.java
@@ -1,5 +1,8 @@
 package com.metacube.shoppingCart.Facade;
 
+import java.util.HashMap;
+import java.util.List;
+
 import com.metacube.shoppingCart.Constants;
 import com.metacube.shoppingCart.Dao.BaseDao;
 import com.metacube.shoppingCart.Dao.FileHandling;
@@ -7,55 +10,73 @@ import com.metacube.shoppingCart.Dao.InMemoryProductDao;
 import com.metacube.shoppingCart.Model.Product;
 import com.metacube.shoppingCart.Model.ShoppingCart;
 
-
 /**
  * The Class ProductFacade.
  */
 public class ProductFacade {
-	
+
 	/** The object. */
-	BaseDao object = ProductFactory.getInstance(FileHandling.read(Constants.FILELOC));
-	
+	BaseDao object = ProductFactory.getInstance(FileHandling
+			.read(Constants.FILELOC));
+
 	/**
 	 * Check product code.
 	 *
-	 * @param input the input
+	 * @param input
+	 *            the input
 	 * @return true, if successful
 	 */
-	public boolean checkProductCode(Product input){
-		if( InMemoryProductDao.productMap.containsKey(input.getCode())){
+	public boolean checkProductCode(String code) {
+		if (getProducts().containsKey(code)) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Gets the cost.
 	 *
-	 * @param input the input
+	 * @param input
+	 *            the input
 	 * @return the cost
 	 */
-	public double getCost(Product input){
+	public double getCost(Product input) {
 		double total;
 		Product prod = (Product) object.retreive(input.getCode());
-		double price = prod.getPrice(); 
+		double price = prod.getPrice();
 		total = input.getQuantity() * price;
 		return total;
 	}
-	
+
 	/**
 	 * Adds the to cart.
 	 *
-	 * @param input the input
+	 * @param input
+	 *            the input
 	 */
-	public void addToCart(Product input){
+	public void addToCart(Product input) {
 		ShoppingCart cart = new ShoppingCart();
 		cart.setCode(input.getCode());
 		cart.setPrice(getCost(input));
 		cart.setQuantity(input.getQuantity());
-		((InMemoryProductDao)object).addToCartItems(cart);
-		System.out.println("Product added successfully to the cart !!");
+		((InMemoryProductDao) object).addToCartItems(cart);
+	}
+
+	public boolean checkProductCodeFromCart(String code) {
+		List<ShoppingCart> cartItems = getCart();
+		for (ShoppingCart item : cartItems) {
+			if (item.getCode().equals(code))
+				return true;
+		}
+		return false;
+	}
+
+	public List<ShoppingCart> getCart() {
+		return ((InMemoryProductDao) object).getCartItems();
+	}
+
+	public HashMap<String, Product> getProducts() {
+		return ((InMemoryProductDao) object).getAllProducts();
 	}
 }
