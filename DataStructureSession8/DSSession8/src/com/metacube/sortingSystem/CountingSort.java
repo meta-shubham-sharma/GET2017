feaf6c04900be70547ab9e962eb5eaf727298c65
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-09-01 15:01:44 +0530

    Add files via upload

--- DataStructureSession8/DSSession8/src/com/metacube/sortingSystem/CountingSort.java
+++ DataStructureSession8/DSSession8/src/com/metacube/sortingSystem/CountingSort.java
@@ -1,6 +1,8 @@
 package com.metacube.sortingSystem;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CountingSort {
 
@@ -9,7 +11,45 @@ public class CountingSort {
 	 * @param array
 	 * @return String of sorted numbers
 	 */
-	public static String sort(int array[]) {
+	public static String CountingSorts(int array[]) {
+
+		int size = array.length - 1, kindex = 0;
+
+		// create 2 lists for positive and negative values
+		List<Integer> list1 = new ArrayList<Integer>();
+		List<Integer> list2 = new ArrayList<Integer>();
+
+		// add numbers to lists for negative and positive values
+		for (int index = 0; index <= size; index++) {
+			if (array[index] < 0) {
+				list1.add(-1 * array[index]);
+			} else {
+				list2.add(array[index]);
+			}
+		}
+
+		// add to negative array only when list of negatives is not empty
+		if (!list1.isEmpty()) {
+			int[] negativeArray = list1.stream().mapToInt(i -> i).toArray();
+			int array1[] = sort(negativeArray);
+			for (int index = array1.length - 1; index >= 0; index--) {
+				array[kindex++] = -1 * array1[index];
+			}
+		}
+
+		// add to positive array only when list of positives is not empty
+		if (!list2.isEmpty()) {
+			int[] positiveArray = list2.stream().mapToInt(i -> i).toArray();
+			int array2[] = sort(positiveArray);
+			for (int index = 0; index < array2.length; index++) {
+				array[kindex++] = array2[index];
+			}
+		}
+
+		return Arrays.toString(array);
+	}
+
+	public static int[] sort(int array[]) {
 
 		int size = array.length - 1;
 		int output[] = new int[size + 1];
@@ -43,7 +83,6 @@ public class CountingSort {
 		for (int index = 0; index <= size; index++) {
 			array[index] = output[index];
 		}
-
-		return Arrays.toString(array);
+		return array;
 	}
 }
