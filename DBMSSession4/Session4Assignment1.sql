8bb1c47e9b84bd17a24f482b873e12715c96107e
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-09-12 12:26:39 +0530

    Update Session4Assignment1.sql

--- DBMSSession4/Session4Assignment1.sql
+++ DBMSSession4/Session4Assignment1.sql
@@ -10,16 +10,16 @@ WHERE member_nm != 'gourav' AND category = (SELECT category
 SELECT mb.member_nm,t.title_nm,i.issue_dt,i.due_dt
 FROM book_issue i JOIN book_return r ON r.return_dt IS NULL AND i.accession_no = r.accession_no AND i.member_id = r.member_id 
 join books b on r.accession_no = b.accession_no 
-JOIN titles t ON b.title_id = t.title_id 
-join members mb
+JOIN titles t ON b.title_id = t.title_id, 
+members mb
 WHERE EXISTS (SELECT mb.member_nm FROM members WHERE r.member_id = mb.member_id ); 
 
 /* Assignment 1.3 select details of books issued for more than due date */
 SELECT mb.member_nm,t.title_nm,i.issue_dt,i.due_dt
 FROM book_issue i JOIN book_return r ON i.accession_no = r.accession_no AND i.member_id = r.member_id 
 join books b on r.accession_no = b.accession_no 
-JOIN titles t ON b.title_id = t.title_id 
-join members mb 
+JOIN titles t ON b.title_id = t.title_id, 
+members mb 
 WHERE DATEDIFF(r.return_dt,i.due_dt) >=1 AND EXISTS (SELECT mb.member_nm FROM members WHERE r.member_id = mb.member_id ) ;
 
 /* Assignment 1.4 select books of a particular price*/
