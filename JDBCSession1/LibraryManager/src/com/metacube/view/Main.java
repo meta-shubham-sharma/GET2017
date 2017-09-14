47be513963c4e30e77b76f0910c67d3045afd1a1
meta-shubham-sharma <shubham.sharma@metacube.com>
2017-09-14 17:39:28 +0530

    Add files via upload

--- JDBCSession1/LibraryManager/src/com/metacube/view/Main.java
+++ JDBCSession1/LibraryManager/src/com/metacube/view/Main.java
@@ -4,26 +4,31 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
-import com.metacube.dao.HelperDao;
+import com.metacube.dao.DBHelper;
 import com.metacube.model.Titles;
 
 public class Main {
 	public static void main(String[] args) throws SQLException {
 		Scanner scan = new Scanner(System.in);
-		HelperDao connect = new HelperDao();
+		DBHelper object = new DBHelper();
 		do {
 			System.out.println("\n1. Get all books by authors's name");
 			System.out
 					.println("2. Check if book has been issued based on book's name");
-			System.out
-					.println("3. Delete all boooks not issued in last 1 year");
+			System.out.println("3. Delete all books not issued in last 1 year");
 			System.out.println("4. Exit");
-			int choice = Integer.parseInt(scan.nextLine());
+			System.out.println("Enter your choice");
+			int choice = 0;
+			try {
+				choice = Integer.parseInt(scan.nextLine());
+			} catch (NumberFormatException exc) {
+				System.out.println("Enter a valid integral choice " + exc);
+			}
 			switch (choice) {
 			case 1:
 				System.out.println("Enter the name of the author");
 				String input = scan.nextLine();
-				List<Titles> list = connect.getBookDetails(input);
+				List<Titles> list = object.getBookDetailsByAuthor(input);
 				if (list.isEmpty()) {
 					System.out
 							.println("This Author has no books in library !!");
@@ -36,7 +41,7 @@ public class Main {
 			case 2:
 				System.out.println("Enter the name of the book");
 				input = scan.nextLine();
-				int output = connect.checkBook(input);
+				int output = object.checkBook(input);
 				if (output == 1) {
 					System.out.println("Book can be issued !!");
 				} else if (output == 0) {
@@ -46,13 +51,15 @@ public class Main {
 				}
 				break;
 			case 3:
-				int rows = connect.deleteBooks();
-				System.out.println("Number of books deleted are : " + rows);
+				int numOfBooksDeleted = object.deleteBooks();
+				System.out.println("Number of books deleted are : " + numOfBooksDeleted);
 				break;
 			case 4:
-				connect.closeConnection();
+				object.closeConnection();
 				scan.close();
 				System.exit(0);
+			default:
+				System.out.println("Enter a choice between 1-4");
 			}
 		} while (true);
 	}
